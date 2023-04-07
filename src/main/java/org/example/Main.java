dc04b9409a9926986f582f05c73b3659eaee78c3
Oleg Chumin <tschumin.oleg@gmail.com>
2023-04-07 10:32:40 +0300

    Add method getGreeting() in Main Create MainTest Add Junit dependencies in pom.xml

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,21 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
+    private String greeting;
+
+    public Main() {
+        this.greeting = "Hello and welcome!";
+    }
+
+    public String getGreeting() {
+        return this.greeting;
+    }
+
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Starting application...%n");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,2 +1,15 @@
+import org.example.Main;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MainTest {
+
+    @Test
+    public void testMain() {
+        Main main = new Main();
+        String expectedOutput = "Hello and welcome!";
+        String actualOutput = main.getGreeting();
+        assertEquals(expectedOutput, actualOutput);
+    }
 }
