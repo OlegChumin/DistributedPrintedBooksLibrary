5bedddb2dad8e62b8df10bf01f523dfd9039e20a
Oleg Chumin <tschumin.oleg@gmail.com>
2023-04-07 20:16:39 +0300

    Add spring-boot-starter-thymeleaf dependency

---
+++ src/main/java/com/epam/library/Application.java
@@ -0,0 +1,12 @@
+package com.epam.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
